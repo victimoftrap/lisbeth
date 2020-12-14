603167042b7c69463fa24a4782a2714179d32be3
victimoftrap <monsquizo@gmail.com>
2020-12-14 12:38:21 +0700

    Added ability to send events to server

--- src/background.js
+++ src/background.js
@@ -1,29 +1,48 @@
 import { START_TRACKING, STOP_TRACKING } from './trackingTypes';
 import { USER_EVENTS } from './userEventTypes';
+import { EXTENSION_API_URL } from './urls';
 
 const storeEventMessage = (message) => {
     console.log(message);
 };
 
+const sendMessageToServer = (message) => {
+    const request = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message),
+    };
+
+    fetch(EXTENSION_API_URL, request)
+    .then(response => response.json())
+    .then(jsonResponse => console.log(JSON.stringify(jsonResponse)))
+};
+
 const onCreatedTab = (tab) => {
-    storeEventMessage({
+    const eventRequest = {
         type: USER_EVENTS.TAB_EVENT,
         url: tab.url,
         title: tab.title,
         active: tab.active,
         incognito: tab.incognito,
-    })
+    };
+    storeEventMessage(eventRequest);
+    sendMessageToServer(eventRequest);
 };
 
 const onUpdatedTab = (tabId, info, tab) => {
     if (tab.status === 'complete') {
-        storeEventMessage({
+        const eventRequest = {
             type: USER_EVENTS.TAB_EVENT,
             url: tab.url,
             title: tab.title,
             active: tab.active,
             incognito: tab.incognito,
-        });
+        };
+        storeEventMessage(eventRequest);
+        sendMessageToServer(eventRequest);
     }
 };
 
@@ -47,6 +66,7 @@ const handleMessage = (message) => {
             break;
         default:
             storeEventMessage(message);
+            sendMessageToServer(message);
             break;
     }
 };
---
+++ src/urls.js
@@ -0,0 +1,3 @@
+const LOCAL_API_URL = 'http://localhost/api/events/save'
+
+export const EXTENSION_API_URL = LOCAL_API_URL;
