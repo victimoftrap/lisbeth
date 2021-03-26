daa01ab4460167077f5d364225dcf1944a32458b
victimoftrap <monsquizo@gmail.com>
2021-03-26 15:07:51 +0700

    Added event with solution source code

---
+++ src/codeGrabberConstants.js
@@ -0,0 +1,2 @@
+export const GRAB_ON_RELOAD = 'on-reload';
+export const NO_GRAB = 'nothing';
--- src/content.js
+++ src/content.js
@@ -1,15 +1,22 @@
 import { START_TRACKING, STOP_TRACKING } from './trackingTypes';
 import { USER_EVENTS } from './userEventTypes';
+import { GRAB_ON_RELOAD, NO_GRAB } from './codeGrabberConstants';
 
 import getCookie from './cookie';
 import { currentDatetime } from './utils'
 
 const LISBETH_STATE = 'lisbeth-state';
+const LISBETH_GRAB = 'lisbeth-grab';
 
+// getting contest ID from browser's pathname
 const YANDEX_CONTEST_ID = parseInt(window.location.pathname.match(new RegExp('(contest\/)([0-9]+)'))[2]);
+
 // This is string value of 'yandexuid' because its not fits in JS Number type
 // Coverted as BigInt, it have troubles with JSON, so it would be just string
 const YANDEX_USER_ID = getCookie('yandexuid');
+
+// start communication port of content and background script
+let port = chrome.runtime.connect({ name: 'lisbeth' });
 
 const sendTrackingMessage = (type, event) => {
     port.postMessage({
@@ -54,13 +61,52 @@ const initContentListeners = () => {
         });
     }
 
+    const isCodingProblem = document.getElementsByClassName('solution_type_compiler-list').length ? true : false;
     const sentProblemButton = document.getElementsByClassName('button_role_submit')[0];
     sentProblemButton.addEventListener('click', (event) => {
+        if (isCodingProblem) {
+            localStorage.setItem(LISBETH_GRAB, GRAB_ON_RELOAD);
+        }
+
         const problemTitle = document.getElementsByClassName('title')[0];
         sendTrackingMessage(USER_EVENTS.PROBLEM_SENT_EVENT, {
             title: problemTitle.innerText,
         });
     });
+
+    const grab = localStorage.getItem(LISBETH_GRAB);
+    if (grab === GRAB_ON_RELOAD) {
+        localStorage.setItem(LISBETH_GRAB, NO_GRAB);
+        grabSolutionCode();
+    }
+};
+
+const grabSolutionCode = () => {
+    const problemTitle = document.getElementsByClassName('title')[0];
+
+    const compilerOptionsSelector = document.getElementsByClassName('select_role_compiler-list');
+    let compilerName = "";
+    if (compilerOptionsSelector.length != 0) {
+        const compilerCollections = compilerOptionsSelector[0].querySelector('select').querySelectorAll('option');
+        for (const compiler of compilerCollections) {
+            if (compiler.selected) {
+                compilerName = compiler.innerText;
+            }
+        }
+    }
+    
+    const solutionType = localStorage.getItem('solutionType');
+    const sourceCode = document.getElementsByClassName('input__control')[0].innerText;
+
+    const message = {
+        problemTitle: problemTitle.innerText,
+        problemUrl: window.location.pathname,
+        compiler: compilerName,
+        solutionType: solutionType,
+        source: sourceCode,
+    };
+    console.log(message);
+    sendTrackingMessage(USER_EVENTS.SOLUTION_CODE_SENT_EVENT, message);
 };
 
 const disableContentListeners = () => {
@@ -84,7 +130,6 @@ const checkAppState = () => {
 
 checkAppState();
 
-let port = chrome.runtime.connect({ name: 'lisbeth' });
 const startContestButtons = document.getElementsByClassName('button_theme_action')[0];
 const endContestButtons = document.getElementsByClassName('button_role_end')[0];
 
